5f7979014c1a437da1ef31978931ce9fe870bdff
sanchico24 <111749678+sanchico24@users.noreply.github.com>
2022-09-20 00:20:30 +0700

    Delete 13.Platform_Widget directory

--- 13.Platform_Widget/praktikum/android/app/src/main/kotlin/com/example/praktikum12/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.praktikum12
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- 13.Platform_Widget/praktikum/lib/models/drawer_item.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawerItem {
-  final IconData iconData;
-  final String title;
-
-  DrawerItem({required this.iconData, required this.title});
-}
